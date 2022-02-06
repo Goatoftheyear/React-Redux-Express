8f068664d46e2de87c01a816c4ffbc34f646857a
Goatoftheyear <lim.px100@gmail.com>
2022-02-06 21:18:40 +0800

    create payment button and create charge

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const keys = require("./config/keys");
 //need to require these files to get their components
 //if not it was treated as non-existant
 //kinda like loading other things needed
+const bodyParser = require("body-parser");
 require("./models/User");
 //is below here cos it uses User which is sth above
 //if used below it will cause error
@@ -21,6 +22,8 @@ mongoose.connect(keys.mongoURI, {
 });
 
 const app = express();
+
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     //how long the cookie exist until it expired
@@ -39,5 +42,6 @@ app.use(passport.session());
 //said function required a parameter which is app
 //sth like function(app)
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
