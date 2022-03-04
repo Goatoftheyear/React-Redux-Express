bb6d6a687bc265f0bf35e782ab9fa862ca22b924
Goatoftheyear <lim.px100@gmail.com>
2022-03-04 21:08:33 +0800

    change heroku production env

--- config/keys.js
+++ config/keys.js
@@ -4,6 +4,6 @@ if (process.env.NODE_ENV === "production") {
   //we are in production - return prod set of keys
   module.exports = require("./prod");
 } else {
-  //we are in development - return the keys
+  //we are in development - return the keys kekw
   module.exports = require("./dev");
 }
