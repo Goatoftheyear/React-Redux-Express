b104577f93537bfd80d69ad25f635afe8ef196fb
Goatoftheyear <lim.px100@gmail.com>
2022-07-29 20:51:57 +0800

    add icons and fields for survey form
    used icons and button from materialize and fields for survey form

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,14 @@
+// SurveyFied contains logic to render a single
+// label and text input
+import React from "react";
+
+export default ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
+
+//<Field type="text" name="surveyTitle" component="input" />
