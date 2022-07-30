2f5bbf6f500e4636b3d5690e8887595eadbc6623
Goatoftheyear <lim.px100@gmail.com>
2022-07-30 18:16:54 +0800

    add in form review
    add in form review and make data persist between review and form itself.
    also add action creator with the api call to deduct the credits when email is sent

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,12 +1,17 @@
 // SurveyFied contains logic to render a single
 // label and text input
 import React from "react";
-
-export default ({ input, label }) => {
+// {touched && error}
+// if touched is true and error contains anything
+// it returns the string
+export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
-      <input {...input} />
+      <input {...input} style={{ marginBottom: "5px" }} />
+      <div className="red-text" style={{ marginBottom: "20px" }}>
+        {touched && error}
+      </div>
     </div>
   );
 };
